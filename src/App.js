4f305c7b9c9eac5077b27854b22825da5de201ce
John Dawes <jdawes@bluebitmedia.com>
2024-06-10 16:56:52 -0600

    Home page CSS scaffolding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Men',
+    },
+  ];
+
   return (
-    <div className="App">
-      Hello World!
+    <div className='categories-containter'>
+      {categories.map((category) => (
+          <div className='category-container' key={category.id}>
+            <div className='category-bg-img' />
+            <div className='category-body-container'>
+              <h2>{category.title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+      ))}
     </div>
   );
 }
