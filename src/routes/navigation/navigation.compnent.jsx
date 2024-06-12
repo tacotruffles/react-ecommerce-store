15dc1fd8fd034c1f57ebce9456d95fed7bfce263
John Dawes <jdawes@bluebitmedia.com>
2024-06-12 12:44:58 -0600

    Firebase App

---
+++ src/routes/navigation/navigation.compnent.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as NavLogo } from '../../assets/crown.svg';
+
+import './navigation.sytles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <NavLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>Shop</Link>
+          <Link className='nav-link' to='/sign-in'>Sign In</Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+}
+
+export default Navigation
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,5 @@
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+}
+
+export default Shop;
